a564c3b58fcc9f71800ad0499012c50f8f867387
caleb <paracordcaleb@gmail.com>
2025-03-24 12:33:13 -0400

    Reorganized and commented most of the written code

--- homework/hw03/js/main.js
+++ homework/hw03/js/main.js
@@ -6,21 +6,33 @@ let password = "password";
 
 async function initializeScreen() {
     token = await getToken();
-    showNav();
+
+    renderNav();
     getUserData();
     getSuggestions();
     getStories();
-
     getPosts();
 }
+
+/*
+ * ###################
+ * #    Auth code    #
+ * ###################
+ */
 
 async function getToken() {
     return await getAccessToken(rootURL, username, password);
 }
 
-function showNav() {
+/*
+ * ##################
+ * #    Nav code    #
+ * ##################
+ */
+
+function renderNav() {
     document.querySelector("#nav").innerHTML = `
-    <nav class="flex justify-between py-5 px-9 bg-white border-b fixed w-full top-0">
+        <nav class="flex justify-between py-5 px-9 bg-white border-b fixed w-full top-0">
             <h1 class="font-Comfortaa font-bold text-2xl">Photo App</h1>
             <ul class="flex gap-4 text-sm items-center justify-center">
                 <li><span>${username}</span></li>
@@ -30,6 +42,13 @@ function showNav() {
     `;
 }
 
+/*
+ * ########################
+ * #    User data code    #
+ * ########################
+*/
+
+// Get Data
 async function getUserData() {
   const response = await fetch("https://photo-app-secured.herokuapp.com/api/profile/", {
     method: "GET",
@@ -41,10 +60,11 @@ async function getUserData() {
 
   const data = await response.json();
   //console.log(data);
-  showUserData(data);
+  renderUserData(data);
 }
 
-function showUserData(userData) {
+//
+function renderUserData(userData) {
   const template = `
       <img src="${userData.thumb_url}" alt="${userData.username}'s profile picture" class="rounded-full w-16" />
       <h2 class="font-Comfortaa font-bold text-2xl">${userData.username}</h2>
@@ -55,6 +75,13 @@ function showUserData(userData) {
 
 }
 
+/*
+ * ##########################
+ * #    Suggestions code    #
+ * ##########################
+ */
+
+// Get Data
 async function getSuggestions() {
   const response = await fetch("https://photo-app-secured.herokuapp.com/api/suggestions/", {
     method: "GET",
@@ -69,6 +96,7 @@ async function getSuggestions() {
   renderSuggestions(data);
 }
 
+// Render Suggestions
 function renderSuggestions(suggestions) {
   suggestions.forEach(renderSuggestion)
 }
@@ -90,6 +118,13 @@ function renderSuggestion(suggestion) {
 
 }
 
+/*
+ * ######################
+ * #    Stories code    #
+ * ######################
+ */
+
+// Get Data
 async function getStories() {
   const response = await fetch("https://photo-app-secured.herokuapp.com/api/stories/", {
     method: "GET",
@@ -104,6 +139,7 @@ async function getStories() {
   renderStories(data);
 }
 
+// Render Stories
 function renderStories(stories) {
   stories.forEach(renderStory)
 }
@@ -121,8 +157,13 @@ function renderStory(story) {
 
 }
 
-// implement remaining functionality below:
-//await / async syntax:
+/*
+ * ####################
+ * #    Posts code    #
+ * ####################
+ */
+
+// Get Data
 async function getPosts() {
     const response = await fetch("https://photo-app-secured.herokuapp.com/api/posts/?limit=10", {
         method: "GET",
@@ -136,6 +177,163 @@ async function getPosts() {
     renderPosts(data);
 }
 
+// Render Posts
+function renderPosts(postListJSON) {
+  postListJSON.forEach(renderPost);
+}
+
+function renderPost(postJSON) {
+  const template = `
+    <section class="bg-white border mb-10">
+      <div class="p-4 flex justify-between">
+        <h3 class="text-lg font-Comfortaa font-bold">${postJSON.user.username}</h3>
+        <button aria-label="post options" class="icon-button">
+          <i class="fas fa-ellipsis-h"></i>
+        </button>
+      </div>
+      <img
+        src="${postJSON.image_url}"
+        alt="${postJSON.alt_text}"
+        width="300"
+        height="300"
+        class="w-full bg-cover"
+      />
+      <div class="p-4">
+        <div class="flex justify-between text-2xl mb-3">
+          <div>
+            ${renderLikeButton(postJSON)}
+            <button aria-label="view comment section">
+              <i class="far fa-comment"></i>
+            </button>
+            <button aria-label="share post">
+              <i class="far fa-paper-plane"></i>
+            </button>
+          </div>
+          <div>${renderBookmarkButton(postJSON)}</div>
+        </div>
+        <p class="font-bold mb-3">${postJSON.likes.length} likes</p>
+        <div class="text-sm mb-3">
+          <p>
+            <strong>${postJSON.user.username}</strong>
+            ${postJSON.caption}
+          </p>
+        </div>
+        <p class="uppercase text-gray-500 text-xs">${postJSON.display_time}</p>
+        ${renderCommentSection(postJSON)}
+      </div>
+      <div class="flex justify-between items-center p-3">
+        <div class="flex items-center gap-3 min-w-[80%]">
+          <i class="far fa-smile text-lg"></i>
+          <input
+            aria-label="add a comment"
+            type="text"
+            class="min-w-[80%] focus:outline-none"
+            placeholder="Add a comment..."
+          />
+        </div>
+        <button aria-label="post comment" class="text-blue-500 py-2">Post</button>
+      </div>
+    </section>
+  `;
+
+  const container = document.querySelector('main');
+  container.insertAdjacentHTML('beforeend', template);
+}
+
+/*
+ * ##############################
+ * #    Comment Section code    #
+ * ##############################
+ */
+
+// Add comment
+/*
+ * TODO: add comments
+ */
+
+// Render Comment Section
+function renderCommentSection(postJSON) {
+  let template = '';
+  let commentNum = Object.keys(postJSON.comments).length;
+
+  if (commentNum > 1) {
+    template = `
+    <button aria-label="view all comments" class="text-blue-500 text-sm py-2">View all ${commentNum} comments</button>
+    <p class="text-sm mb-3">
+    <strong>${postJSON.comments[commentNum - 1].user.username}</strong>
+    ${postJSON.comments[commentNum - 1].text}
+    </p>
+    <p class="uppercase text-gray-500 text-xs">${postJSON.comments[commentNum - 1].display_time}</p>
+    `;
+
+  } else if (commentNum === 1){
+    template = `
+    <p class="text-sm mb-3">
+    <strong>${postJSON.comments[commentNum - 1].user.username}</strong>
+    ${postJSON.comments[commentNum - 1].text}
+    </p>
+    <p class="uppercase text-gray-500 text-xs">${postJSON.comments[commentNum - 1].display_time}</p>
+    `;
+
+  }
+
+  return template
+
+}
+
+/*
+ * #######################
+ * #    Bookmark code    #
+ * #######################
+ */
+
+// TODO: I assume we don't actually need this so probably remove
+// Get Data
+async function getBookmarks() {
+  const response = await fetch("https://photo-app-secured.herokuapp.com/api/bookmarks/", {
+    method: "GET",
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`
+    }
+  });
+  const data = await response.json();
+  console.log(data);
+}
+
+// Create Bookmark
+window.createBookmark = async function (post_id) {
+  const postData = {
+    "post_id": post_id
+  };
+
+  const response = await fetch("https://photo-app-secured.herokuapp.com/api/bookmarks/", {
+    method: "POST",
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`
+    },
+    body: JSON.stringify(postData)
+  });
+  const data = await response.json();
+  console.log(data);
+}
+
+// Remove Bookmark
+window.removeBookmark = async function (post_id) {
+  const response = await fetch(`https://photo-app-secured.herokuapp.com/api/bookmarks/${post_id}`, {
+    method: "DELETE",
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`
+    }
+  });
+  const data = await response.json();
+  console.log(data);
+}
+
+// TODO: change button to auto redraw element
+// Render Bookmark
 function renderBookmarkButton(postJSON) {
   let template = '';
 
@@ -155,6 +353,46 @@ function renderBookmarkButton(postJSON) {
   return template;
 }
 
+/*
+ * ###################
+ * #    Like code    #
+ * ###################
+ */
+
+// Create Like
+window.addLike = async function (post_id) {
+  const postData = {
+    "post_id": post_id
+  };
+
+  const response = await fetch("https://photo-app-secured.herokuapp.com/api/likes/", {
+    method: "POST",
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`
+    },
+    body: JSON.stringify(postData)
+  });
+  const data = await response.json();
+  console.log(data);
+}
+
+// Remove Like
+window.removeLike = async function (post_id) {
+  console.log("AAAAAAHHHHH WHY DO YOU HATE ME :(")
+  const response = await fetch(`https://photo-app-secured.herokuapp.com/api/likes/${post_id}`, {
+    method: "DELETE",
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`
+    }
+  });
+  const data = await response.json();
+  console.log(data);
+}
+
+// TODO: change button to auto redraw element
+// Render Like button
 function renderLikeButton(postJSON) {
   let template = '';
 
@@ -162,170 +400,35 @@ function renderLikeButton(postJSON) {
     template = `
       <button aria-label="remove like from post" onclick="window.removeLike(${postJSON.current_user_like_id})">
           <i class="text-red-600 fas fa-heart"></i>
-      </button>`;
+      </button>
+      `;
     
   } else {
     template = `
       <button aria-label="add like to post" onclick="window.addLike(${postJSON.id})">
           <i class="far fa-heart"></i>
-      </button>`;
+      </button>
+      `;
   }
 
   return template;
 }
 
-function getCommentSection(postJSON) {
-  let template = '';
-  let commentNum = Object.keys(postJSON.comments).length;
+/*
+ * #####################
+ * #    Follow code    #
+ * #####################
+ */
 
-  if (commentNum > 1) {
-      template = `<button aria-label="view all comments" class="text-blue-500 text-sm py-2">View all ${commentNum} comments</button>
-<p class="text-sm mb-3">
-                    <strong>${postJSON.comments[commentNum - 1].user.username}</strong>
-                    ${postJSON.comments[commentNum - 1].text}
-                </p>
-                <p class="uppercase text-gray-500 text-xs">${postJSON.comments[commentNum - 1].display_time}</p>
+// Follow Account
+/*
+ * TODO: Follow account
+ */
 
-      `;
-  } else if (commentNum === 1){
-    template = `<p class="text-sm mb-3">
-                    <strong>${postJSON.comments[commentNum - 1].user.username}</strong>
-                    ${postJSON.comments[commentNum - 1].text}
-                </p>
-                <p class="uppercase text-gray-500 text-xs">${postJSON.comments[commentNum - 1].display_time}</p>
-    `;
-
-  }
-
-  return template
-}
-
-function renderPost(postJSON) {
-  const template = `
-        <!-- Post 1 -->
-        <section class="bg-white border mb-10">
-            <div class="p-4 flex justify-between">
-                <h3 class="text-lg font-Comfortaa font-bold">${postJSON.user.username}</h3>
-                <button aria-label="post options" class="icon-button"><i class="fas fa-ellipsis-h"></i></button>
-            </div>
-            <img src="${postJSON.image_url}" alt="${postJSON.alt_text}" width="300" height="300"
-                class="w-full bg-cover">
-            <div class="p-4">
-                <div class="flex justify-between text-2xl mb-3">
-                    <div>
-                        ${renderLikeButton(postJSON)}
-                        <button aria-label="view comment section"><i class="far fa-comment"></i></button>
-                        <button aria-label="share post"><i class="far fa-paper-plane"></i></button>
-                    </div>
-                    <div>
-                      ${renderBookmarkButton(postJSON)}
-                    </div>
-                </div>
-                <p class="font-bold mb-3">${postJSON.likes.length} likes</p>
-                <div class="text-sm mb-3">
-                    <p>
-                        <strong>${postJSON.user.username}</strong>
-                        ${postJSON.caption}
-                    </p>
-                </div>
-                <p class="uppercase text-gray-500 text-xs">${postJSON.display_time}</p>
-                ${getCommentSection(postJSON)}
-            </div>
-            <div class="flex justify-between items-center p-3">
-                <div class="flex items-center gap-3 min-w-[80%]">
-                    <i class="far fa-smile text-lg"></i>
-                    <input aria-label="add a comment" type="text" class="min-w-[80%] focus:outline-none" placeholder="Add a comment...">
-                </div>
-                <button aria-label="post comment" class="text-blue-500 py-2">Post</button>
-            </div>
-        </section>
-  `;
-  const container = document.querySelector('main');
-  container.insertAdjacentHTML('beforeend', template);
-}
-
-function renderPosts(postListJSON) {
-  postListJSON.forEach(renderPost);
-}
-
-
-//await / async syntax:
-async function getBookmarks() {
-    const response = await fetch("https://photo-app-secured.herokuapp.com/api/bookmarks/", {
-        method: "GET",
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${token}`
-        }
-    });
-    const data = await response.json();
-    console.log(data);
-}
-
-
-//await / async syntax:
-window.createBookmark = async function (post_id) {
-    const postData = {
-      "post_id": post_id
-    };
-
-    const response = await fetch("https://photo-app-secured.herokuapp.com/api/bookmarks/", {
-        method: "POST",
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${token}`
-        },
-        body: JSON.stringify(postData)
-    });
-    const data = await response.json();
-    console.log(data);
-}
-
-//await / async syntax:
-window.removeBookmark = async function (post_id) {
-    const response = await fetch(`https://photo-app-secured.herokuapp.com/api/bookmarks/${post_id}`, {
-        method: "DELETE",
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${token}`
-        }
-    });
-    const data = await response.json();
-    console.log(data);
-}
-
-//await / async syntax:
-window.addLike = async function (post_id) {
-    const postData = {
-      "post_id": post_id
-    };
-
-    const response = await fetch("https://photo-app-secured.herokuapp.com/api/likes/", {
-        method: "POST",
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${token}`
-        },
-        body: JSON.stringify(postData)
-    });
-    const data = await response.json();
-    console.log(data);
-}
-
-//await / async syntax:
-window.removeLike = async function (post_id) {
-    console.log("AAAAAAHHHHH WHY ARE YOU KILLING ME :(")
-    const response = await fetch(`https://photo-app-secured.herokuapp.com/api/likes/${post_id}`, {
-        method: "DELETE",
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${token}`
-        }
-    });
-    const data = await response.json();
-    console.log(data);
-}
-
+// Unfollow Account
+/*
+ * TODO: Unfollow account
+ */
 
 // after all of the functions are defined, invoke initialize at the bottom:
 initializeScreen();
